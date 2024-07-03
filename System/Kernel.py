56a059e296e715d80ff8ca807c2dd8cc33702bb1
Max Hammer <132189974+Master916Max@users.noreply.github.com>
2024-07-03 18:33:07 +0200

    Update Kernel.py
    Almost Finished StartUp Function

--- System/Kernel.py
+++ System/Kernel.py
@@ -3,7 +3,20 @@ import multiprocessing
 
 #Init
 def StartUp()
-  pass
+  from System.KP.SFRP import P4, P2
+  error, code = P4()
+  if code:
+    print(error)
+    exit()
+  else:
+  error, code = None, None
+  error, code = P2()
+  if code:
+    print(error)
+    exit()
+  else:
+    del error
+    del code
 
 def Run()
   pass
